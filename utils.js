510b752bfa4c6b6a069d03f98c9bcbb23fdf7a0e
Yuko <yuko.kumason@gmail.com>
2021-09-02 15:55:47 -0700

    multiplier added

--- utils.js
+++ utils.js
@@ -1,4 +1,11 @@
 function product(num1, num2){
-    return num1 * num2;
+    return num1 + num2;
 }
 console.log(product(3, 4));
+
+
+
+function divide(x, y) {
+    return x * y
+}
+console.log(divide(2, 3));
